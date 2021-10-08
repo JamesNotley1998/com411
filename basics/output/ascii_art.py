cdc30e99a6e8038e0cf21bd653742d0ac176fc2c
James Notley <5notlj93@solent.ac.uk>
2021-10-08 11:52:12 +0100

    Added code to display an ASCII robot with dynamic eyes

--- basics/output/ascii_art.py
+++ basics/output/ascii_art.py
@@ -1,5 +1,5 @@
 # Display a box
-print("----")
-print("-00-")
-print("-__-")
-print("----")
+print("##########")
+print("#        #")
+print("#        #")
+print("##########")
---
+++ basics/output/ascii_robot.py
@@ -0,0 +1,12 @@
+# Display a box
+print("##########")
+print("#        #")
+print("# ______ #")
+print("##########")
+# Please enter a character for the eye
+print("Please enter a character for the eye?")
+name = input()
+print("##########")
+print("#  o  o  #")
+print("# ______ #")
+print("##########")
