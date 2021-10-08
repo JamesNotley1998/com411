7fc628507e2e3638cca76381fde4c26f309bb154
James Notley <5notlj93@solent.ac.uk>
2021-10-08 12:51:29 +0100

    Coding Session - 08.10.2021

--- basics/output/user_input.py
+++ basics/output/user_input.py
@@ -1,8 +1,15 @@
-# Ask user to enter their name
-print("What is your name human?")
-name = input()
-print(f"It is nice to meet you James")
 # Read in user's name
 name = input("What is your name human?")
-print(f"Nice to meet you James")
-
+print(f"Nice to meet you {name}")
+# Ask user how old they are
+print("How old are you (in years)?")
+# Read in users age
+whole_number = int(input())
+#Ask how tall you are
+print("How tall are you (in meters)?")
+decimal_number = float(input())
+#Ask how much they weigh
+print("How much do you weigh (in kilograms)?")
+whole_number = int(input())
+#Calculate BMI
+print(f"{name} you are {str().format(height*height)}")
