d68ec40fcdc8093ef6236046828ca804887f2fd1
James Notley <5notlj93@solent.ac.uk>
2021-10-08 11:39:18 +0100

    Added code to read user input

---
+++ basics/output/user_input.py
@@ -0,0 +1,8 @@
+# Ask user to enter their name
+print("What is your name human?")
+name = input()
+print(f"It is nice to meet you James")
+# Read in user's name
+name = input("What is your name human?")
+print(f"Nice to meet you James")
+
