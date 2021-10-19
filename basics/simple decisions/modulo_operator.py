f9d655961864c50d28867a0517d77e83bf2e4966
James Notley <jamesnotley@Jamess-MacBook-Pro.local>
2021-10-19 15:44:36 +0100

    Added code to demonstrate the use of a modulus operator.

---
+++ basics/simple decisions/modulo_operator.py
@@ -0,0 +1,9 @@
+#Enter whole number
+print("Please enter a whole number.")
+number = int(input())
+
+#Display message / odd or even
+if number % 2 == 0:
+    print(f"The number {number} is an even number.")
+else:
+    print(f"The number {number} is an odd number.")
