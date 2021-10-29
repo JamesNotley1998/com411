e82b76608061b423d4bc7b7b04a14dea9e87f52d
James Notley <jamesnotley@Jamess-MacBook-Pro.local>
2021-10-29 12:24:29 +0100

    Added code to display a simple for loop

---
+++ basics/for_loops/simple.py
@@ -0,0 +1,15 @@
+#How many mountains
+print ("How many mountains should I display?")
+number_of_mountains = int(input())
+
+#for loop
+for mountain in range (number_of_mountains):
+    print ("""
+           __
+          /  \\_  
+         /^    \\
+        /  ^    \\_
+      _/ ^ ^     ^\\
+     /  ^     ^    \\
+
+  """)
